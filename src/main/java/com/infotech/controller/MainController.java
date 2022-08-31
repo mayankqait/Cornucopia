934e25707acf5e398ac13a37a732001ba82519c7
mchauhan-c <mayank.chauhan@qualitestgroup.com>
2022-08-31 18:42:21 +0530

    second commit 22

--- src/main/java/com/infotech/controller/MainController.java
+++ src/main/java/com/infotech/controller/MainController.java
@@ -80,7 +80,7 @@ public class MainController {
 		return (List<Game>)teamServiceImpl.getGamesByTeam(team);
 	}
 	
-	@GetMapping(path="/findGamesByTeam11/{team1}")
+	@GetMapping(path="/findGamesByTeam22/{team2}")
 	public List<Game> getGamesByTeamName(@PathVariable("team") String team){
 		return (List<Game>)teamServiceImpl.getGamesByTeam(team);
 	}
